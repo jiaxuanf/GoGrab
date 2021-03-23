872ce1993c33801877a3e73d98e577394a85f58f
gohjoshua <goh_joshua@u.nus.edu>
2021-03-23 19:52:54 +0800

    Add navigational bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <h1> hi there </h1>
-   
+    <div class="navbar">
+  <a href="#home">List Your Car</a>
+  <a href="#news">Cars</a>
+  <a href="#contact">Messages</a>
+</div>
+   <router-view> </router-view>
   </div>
 </template>
 
@@ -9,22 +14,36 @@
 
 
 export default {
-  methods: {
+  props: {
   },
-  name: 'App',
-  components: {
-    
+  data() {
+    return {
+
+    };
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.navbar {
+  overflow: hidden;
+  background-color: #ded3ff;
+  position: fixed; /* Set the navbar to fixed position */
+  top: 0; /* Position the navbar at the top of the page */
+  width: 100%; /* Full width */
+}
+
+.navbar a {
+  float: left;
+  display: block;
+  color: black;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+.navbar a:hover {
+  background: #ddd;
+  color: red;
 }
 </style>
