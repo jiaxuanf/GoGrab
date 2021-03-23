ace81d9f60bb3e1dc0ed516715412d85adfc817d
gohjoshua <goh_joshua@u.nus.edu>
2021-03-23 16:23:19 +0800

    Setup router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Routes from './routes.js'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+const myRouter = new VueRouter({
+  routes: Routes,
+  mode: 'history'
+});
 
 new Vue({
   render: h => h(App),
