81f2a4f79ff7744dde1a8969943a1ba2a18a94e3
gohjoshua <goh_joshua@u.nus.edu>
2021-03-24 10:19:43 +0800

    Add cover page and title text

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,35 @@
 <template>
   <div id="app">
-    <h1> hi there </h1>
     <div class="navbar">
-  <a href="#home">List Your Car</a>
-  <a href="#news">Cars</a>
-  <a href="#contact">Messages</a>
-</div>
-   <router-view> </router-view>
+      <a href="#">
+        <img v-bind:src="'https://cdn2.iconfinder.com/data/icons/facebook-51/32/FACEBOOK_LINE-01-512.png'"
+      style="width:40px;height:30px;"/></a>
+      <a href="#messages">Messages</a>
+      <a href="#cars">Cars</a>
+      <a href="#feed">Feed</a>
+      <a href="#listcar">List Your Car</a>
+    </div>
+    <div class="main">
+      <img v-bind:src="'https://lh5.googleusercontent.com/qt-iclLn2RAU2kBotZ919RV1uzrRDIf_e-UDqoK2cCW4wF7KrQH2MYFd0-JdPNOVvSbUEkzNp9ArhdgxOQ5-r9t8U1jPzlr9N9kvVHs-GA'"
+      style="width:100%;"/>
+      <h1 id="title">Drive anywhere you want 🚗</h1>
+      <h2 id="subtitle">Book cars from trusted owners today!</h2>
+      <form>
+        <label for="location">Location</label>
+        <input type="text"> 
+        </form>
+    </div>
+    <router-view> </router-view>
   </div>
 </template>
 
 <script>
-
-
 export default {
-  props: {
-  },
+  props: {},
   data() {
-    return {
-
-    };
-  }
-}
+    return {};
+  },
+};
 </script>
 
 <style>
@@ -34,7 +42,7 @@ export default {
 }
 
 .navbar a {
-  float: left;
+  float: right;
   display: block;
   color: black;
   text-align: center;
@@ -45,5 +53,29 @@ export default {
 .navbar a:hover {
   background: #ddd;
   color: red;
+}
+
+.main {
+  margin-top: 60px; /* Add a top margin to avoid content overlay */
+}
+
+#title {
+  position: absolute;
+  top: 12%;
+  left: 30%;
+  transform: translate(-10%, -50%);
+  color: black;
+  font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-size: 60px;
+}
+
+#subtitle {
+  position: absolute;
+  top: 18%;
+  left: 33%;
+  transform: translate(-10%, -50%);
+  color: black;
+  font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-size: 36px;
 }
 </style>
