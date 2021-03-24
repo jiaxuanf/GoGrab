f98aca536dd9a132f52a624af6bb8b964192ab57
gohjoshua <goh_joshua@u.nus.edu>
2021-03-24 11:42:17 +0800

    Add search boxes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div class="navbar">
-      <a href="#">
+      <a href="#profile">
         <img v-bind:src="'https://cdn2.iconfinder.com/data/icons/facebook-51/32/FACEBOOK_LINE-01-512.png'"
       style="width:40px;height:30px;"/></a>
       <a href="#messages">Messages</a>
@@ -14,9 +14,19 @@
       style="width:100%;"/>
       <h1 id="title">Drive anywhere you want 🚗</h1>
       <h2 id="subtitle">Book cars from trusted owners today!</h2>
-      <form>
-        <label for="location">Location</label>
-        <input type="text"> 
+      <form id="mainsearch">
+        <label for="location">
+          <div class="mainsearch_label">Location:</div>
+          </label>
+        <input class="inputs" type="text" >
+        <label for="fromtime">
+          <div class="mainsearch_label">From:</div>
+          </label>
+        <input class="inputs" type="text"  placeholder="DD/MM/YY"> 
+        <label for="totime">
+          <div class="mainsearch_label">To:</div>
+          </label>
+        <input class="inputs" type="text"  placeholder="DD/MM/YY"> 
         </form>
     </div>
     <router-view> </router-view>
@@ -48,6 +58,8 @@ export default {
   text-align: center;
   padding: 14px 16px;
   text-decoration: none;
+  font-size: 20px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
 
 .navbar a:hover {
@@ -78,4 +90,25 @@ export default {
   font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
   font-size: 36px;
 }
+
+#mainsearch {
+  position:absolute;
+  top: 45%;
+  left: 10%;
+  transform: translate(-10%, -50%);
+}
+
+.mainsearch_label {
+  color: whitesmoke;
+  float: left;
+  font-size: 40px;
+  font-family:monospace ;
+}
+
+.inputs {
+  height: 80px;
+  width: 350px;
+  font-size: 40px;
+}
+
 </style>
