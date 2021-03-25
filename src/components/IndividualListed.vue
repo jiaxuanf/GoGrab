2b1cb2ba68cd86397d4289ad8ece97d0eef627ae
alexaDYZ <e0425338@u.nus.edu>
2021-03-25 14:40:24 +0800

    Update IndividualListed.vue

--- src/components/IndividualListed.vue
+++ src/components/IndividualListed.vue
@@ -1,12 +1,35 @@
 <template>
-    
+<div>
+    <h1>Car Model</h1>
+
     <p id='scrolly'>
     <img src="https://cdn11.bigcommerce.com/s-yrkef1j7/images/stencil/1280x1280/products/1503/30661/QQ20190428230137__95937.1556517799.png?c=2" alt="Izoa">
     <img src="https://cdn11.bigcommerce.com/s-yrkef1j7/images/stencil/1280x1280/products/1503/30658/QQ20190428230027__93343.1556517796.png?c=2" alt="Izoa">
     <img src="https://cdn11.bigcommerce.com/s-yrkef1j7/images/stencil/1280x1280/products/1503/30663/QQ20190428230148__69023.1556517796.png?c=2" alt="Izoa">
-
-
     </p>
+  
+
+<div class="flex-container">
+  <div class="flex-child 1">
+    <ul>
+    <h2>Price </h2>
+    <p>Color </p>
+    <p>Age </p> 
+    <p>Defect </p>
+    <p>Available: </p>
+    </ul>
+    </div>
+
+  <div class="flex-child 2">
+    <ul>
+    <h2>UserName's Car: </h2>
+    <p>Discription box</p>
+    <h2>Owner's Rules: </h2>
+    <p>rule box</p>
+    </ul>
+    </div>
+    </div>
+</div>
 </template>
 
 <script>
@@ -18,7 +41,7 @@ export default {
 }
 </script>
 <style scoped>
-#scrolly{
+#scrolly {
             height: 300px;
             width: 800px;
             overflow: auto;
@@ -27,7 +50,7 @@ export default {
             white-space: nowrap
         }
 
-img{
+img {
             width: 525px;
             height: 300px;
             margin: 20px 10px;
@@ -35,4 +58,20 @@ img{
             margin-left: 40px;
             margin-right: 40px;
         }
+ul {
+            text-align: left;
+            margin-left: 25%;
+
+}
+.flex-container {
+    display: flex;
+}
+
+.flex-child {
+    flex: 0.5;
+}  
+
+.flex-child:first-child {
+    margin-left: 10px;
+} 
 </style>
