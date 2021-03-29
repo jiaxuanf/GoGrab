d71543a6810386c8a47bf3c54466528faf9c1cb9
jiaxuanf <jiaxuanfum@gmail.com>
2021-03-29 08:29:07 +0800

    uploading profile picture to firebase
    feat: uploading profile picture to firebase

--- src/components/updateProfile.vue
+++ src/components/updateProfile.vue
@@ -1,20 +1,50 @@
 <template>
   <div>
-    <h1>UPDATE YOUR PROFILE {{this.username}}</h1>
-    <h3>Contact Information</h3>
-    <form>
-      <label for="email">Email:</label><br />
-      <input type="email" placeholder="Email address..." v-model="email" />
-      <br />
-      <label for="phoneNumber">Phone number:</label>
-      <br />
-      <input
-        type="phoneNumber"
-        placeholder="Phone number..."
-        v-model="phoneNumber"
-      />
-    </form>
-    <button v-on:click="updateContact()">Update Information</button>
+    <h1>UPDATE YOUR PROFILE {{ this.username }}</h1>
+    <br>
+    <div id="information">
+      <h3>Contact Information</h3>
+      <form>
+        <label for="email">Email:</label><br />
+        <input type="email" placeholder="Email address..." v-model="email" />
+        <br />
+        <label for="phoneNumber">Phone number:</label>
+        <br />
+        <input
+          type="phoneNumber"
+          placeholder="Phone number..."
+          v-model="phoneNumber"
+        />
+      </form>
+      <button v-on:click="updateContact()">Update Information</button>
+    </div>
+    <div id="photo">
+      <v-layout row>
+        <v-flex md6 offset-sm3>
+          <div>
+            <div>
+              <v-btn @click="click1">choose a photo</v-btn>
+              <input
+                type="file"
+                ref="input1"
+                style="display: none"
+                @change="previewImage"
+                accept="image/*"
+              />
+            </div>
+            <div v-if="imageData != null">
+              <img class="preview" height="268" width="356" :src="img1" />
+              <br />
+            </div>
+          </div>
+        </v-flex>
+      </v-layout>
+      <v-layout row>
+        <v-flex class="text-center">
+          <v-btn color="pink" @click="uploadPhoto()">upload</v-btn>
+        </v-flex>
+      </v-layout>
+    </div>
   </div>
 </template>
 
@@ -25,20 +55,29 @@ import database from "../main.js";
 export default {
   data() {
     return {
-      username:"",
+      username: "",
       email: "",
       phoneNumber: "",
+      img1: "",
+      imageData: "",
+      uploadValue: 0,
     };
   },
   methods: {
     updateContact() {
       var user = firebase.auth().currentUser;
       var uid = user.uid;
-      database.collection("userInfo").doc(uid).update({
-        email: this.email,
-        phoneNumber: this.phoneNumber,
-      }).then(() => {this.$router.push({
-            path: '/'})
+      database
+        .collection("userInfo")
+        .doc(uid)
+        .update({
+          email: this.email,
+          phoneNumber: this.phoneNumber,
+        })
+        .then(() => {
+          this.$router.push({
+            path: "/",
+          });
         });
     },
     fetchItems: function () {
@@ -54,6 +93,55 @@ export default {
           this.phoneNumber = snapshot.data().phoneNumber;
         });
     },
+    uploadPhoto() {
+      const post = {
+        photo: this.img1,
+      };
+      firebase
+        .database()
+        .ref("PhotoGallery")
+        .push(post)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    click1() {
+      this.$refs.input1.click();
+    },
+    previewImage(event) {
+      this.uploadValue = 0;
+      this.img1 = null;
+      this.imageData = event.target.files[0];
+      this.onUpload();
+    },
+
+    onUpload() {
+      this.img1 = null;
+      const storageRef = firebase
+        .storage()
+        .ref(`${this.imageData.name}`)
+        .put(this.imageData);
+      storageRef.on(
+        `state_changed`,
+        (snapshot) => {
+          this.uploadValue =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        },
+        (error) => {
+          console.log(error.message);
+        },
+        () => {
+          this.uploadValue = 100;
+          storageRef.snapshot.ref.getDownloadURL().then((url) => {
+            this.img1 = url;
+            console.log(this.img1);
+          });
+        }
+      );
+    },
   },
   created() {
     this.fetchItems();
@@ -63,10 +151,12 @@ export default {
 
 <style scoped>
 input {
-  margin-right: 20px;
+  border: 2px solid black;
+  border-radius: 4px;
 }
 h1 {
   font-size: "60px";
+  text-align: center;
 }
 
 form {
@@ -74,5 +164,15 @@ form {
 }
 label {
   color: black;
+}
+
+#information {
+  float: left;
+  width: 50%;
+  padding: 20px;
+}
+#photo {
+  float: right;
+  width: 50%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import { BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import vuetify from './plugins/vuetify';
 
 Vue.use(VueRouter)
 Vue.use(BootstrapVue)
@@ -35,5 +36,6 @@ const myRouter =  new VueRouter({
 
 new Vue({
   render: h => h(App),
+  vuetify,
   router: myRouter
 }).$mount('#app')
