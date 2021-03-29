32f51bc6f99eb612177c0876672ca921b7aa77ab
fsayhan123 <fsayhan123@gmail.com>
2021-03-29 18:53:35 +0800

    Fix: Firebase Errors from fetching data

--- src/main.js
+++ src/main.js
@@ -3,17 +3,17 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import Routescomp from './routes.js'
 import firebase from "firebase"
-
 import { BootstrapVue, IconsPlugin} from 'bootstrap-vue'
-
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import vuetify from './plugins/vuetify';
 
-Vue.use(VueRouter)
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
-Vue.config.productionTip = false
+
+require("firebase/firestore");
+Vue.use(VueRouter);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.config.productionTip = false;
 
 
 const firebaseConfig = {
@@ -23,17 +23,19 @@ const firebaseConfig = {
   storageBucket: "gograb-4c451.appspot.com",
   messagingSenderId: "80331420725",
   appId: "1:80331420725:web:e930a264c0e3447f236d7c",
-  measurementId: "G-W7Z3B5B7MF"
+  measurementId: "G-W7Z3B5B7MF" 
 };
 
-const fb = firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 const database = firebase.firestore();
-export default {fb, database}
+
+
+export default database;
 
 const myRouter =  new VueRouter({
   routes:Routescomp,
   mode:'history'
-})
+});
 
 new Vue({
   render: h => h(App),
