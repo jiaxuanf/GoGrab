132d627a7e6e5617c186d36421fcf4b4a76d4dd8
gohjoshua <goh_joshua@u.nus.edu>
2021-04-09 23:32:26 +0800

    feat: add % labels to chart
    - Percentage labels given to doughnut chart

--- src/components/MyRentals.vue
+++ src/components/MyRentals.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
     <ol>
-      <h1>My Rentals </h1>
-      <br>
-      <h2 v-if="listingsArray.length == 0">No rentals yet! Rent some cars today! 🚘</h2>
+      <h1>My Rentals</h1>
+      <br />
+      <h2 v-if="listingsArray.length == 0">
+        No rentals yet! Rent some cars today! 🚘
+      </h2>
       <li id="listing" v-for="(listing, index) in listingsArray" :key="index">
         {{ listing[1].model }} <br />
         <img />
         <br />
-        DATE: {{ listing[1].afrom }} -- {{ listing[1].ato }}<br />${{ listing[1].price }}
+        DATE: {{ listing[1].afrom }} -- {{ listing[1].ato }}<br />${{
+          listing[1].price
+        }}
         <br />
         <button
           id="reviewButton"
           v-on:click="
             listing_ID = listing[0];
-            owner_ID = listing[1].ownerID
+            owner_ID = listing[1].ownerID;
             goReview();
           "
           v-if="!listing[1].reviewerID"
@@ -30,20 +34,19 @@
 import firebase from "firebase";
 
 export default {
-  components: { },
+  components: {},
   data() {
     return {
       listingsArray: [],
       listing_ID: "",
-      owner_ID: ""
+      owner_ID: "",
     };
   },
   methods: {
     fetchListings: function () {
-      // console.log("userID: " + user.uid);
-const user = firebase.auth().currentUser;
-console.log("current user: " + user.uid);
-     //current logged in user
+      const user = firebase.auth().currentUser;
+      console.log("current user: " + user.uid);
+      //current logged in user
       firebase
         .firestore()
         .collection("listings")
@@ -65,17 +68,17 @@ console.log("current user: " + user.uid);
       console.log("listing_ID: " + this.listing_ID);
       console.log("owner_ID: " + this.owner_ID);
       this.$router.push({
-        name: 'reviewForm',
+        name: "reviewForm",
         params: {
           listingID: this.listing_ID,
-          ownerID: this.owner_ID
-        }
+          ownerID: this.owner_ID,
+        },
       });
     },
   },
   created: function () {
-const user = firebase.auth().currentUser;
-console.log(user.uid);
+    const user = firebase.auth().currentUser;
+    console.log(user.uid);
     this.fetchListings();
     console.log("after created functions");
   },
--- src/components/charts/DoughnutChart.js
+++ src/components/charts/DoughnutChart.js
@@ -1,21 +1,22 @@
 import { Doughnut } from "vue-chartjs";
 import firebase from "firebase";
+import "chartjs-plugin-labels";
 
 export default {
   extends: Doughnut,
   data: function() {
     return {
       datacollection: {
-        labels: ["1-star", "2-star", "3-star", "4-star", " 5-star"],
+        labels: ["1⭐", "2⭐", "3⭐", "4⭐", " 5⭐"],
         datasets: [
           {
             label: "",
             backgroundColor: [
-              "#3e95cd",
-              "#8e5ea2",
-              "#3cba9f",
-              "#e8c3b9",
-              "#c45850",
+              "#cd3e3e",
+              "#cd623e",
+              "#e3e300",
+              "#75cd3e",
+              "#3ecd51",
             ],
             data: [],
           },
@@ -25,12 +26,19 @@ export default {
         title: {
           display: true,
           text: "Review Statistics",
-          fontSize: 40
+          fontSize: 40,
         },
         legend: {
           labels: {
-            fontSize: 20
-          }
+            fontSize: 25,
+          },
+        },
+        plugins: {
+          labels: [
+            {
+              render: "percentage",
+            },
+          ],
         },
         responsive: true,
         maintainAspectRatio: false,
@@ -45,7 +53,7 @@ export default {
     };
   },
   methods: {
-    fetchRatings:  async function() {
+    fetchRatings: async function() {
       const user = firebase.auth().currentUser;
       console.log("current user inside Dashboard: " + user.uid);
       await firebase
@@ -55,32 +63,30 @@ export default {
         .get()
         .then((snapshot) => {
           // var temp = [];
-          snapshot
-            .forEach((doc) => {
-              console.log(doc.id + " ==>" + doc.data());
-              //   if-else for various ratings, update datacollection data
-              if (doc.data().reviewValue == 1) {
-                this.chartData.oneStar += 1;
-              } else if (doc.data().reviewValue == 2) {
-                this.chartData.twoStar += 1;
-                console.log("twoStar incremented: " + this.chartData.twoStar);
-              } else if (doc.data().reviewValue == 3) {
-                this.chartData.threeStar += 1;
-                console.log("threeStar incremented: " + this.chartData.threeStar);
-              } else if (doc.data().reviewValue == 4) {
-                this.chartData.fourStar += 1;
-                console.log("fourStar incremented: " + this.chartData.fourStar);
-              } else {
-                this.chartData.fiveStar += 1;
-              }
-            })
-            
-        })
-        this.datacollection.datasets[0].data.push(this.chartData.oneStar);
-        this.datacollection.datasets[0].data.push(this.chartData.twoStar);
-        this.datacollection.datasets[0].data.push(this.chartData.threeStar);
-        this.datacollection.datasets[0].data.push(this.chartData.fourStar);
-        this.datacollection.datasets[0].data.push(this.chartData.fiveStar);
+          snapshot.forEach((doc) => {
+            console.log(doc.id + " ==>" + doc.data());
+            //   if-else for various ratings, update datacollection data
+            if (doc.data().reviewValue == 1) {
+              this.chartData.oneStar += 1;
+            } else if (doc.data().reviewValue == 2) {
+              this.chartData.twoStar += 1;
+              console.log("twoStar incremented: " + this.chartData.twoStar);
+            } else if (doc.data().reviewValue == 3) {
+              this.chartData.threeStar += 1;
+              console.log("threeStar incremented: " + this.chartData.threeStar);
+            } else if (doc.data().reviewValue == 4) {
+              this.chartData.fourStar += 1;
+              console.log("fourStar incremented: " + this.chartData.fourStar);
+            } else {
+              this.chartData.fiveStar += 1;
+            }
+          });
+        });
+      this.datacollection.datasets[0].data.push(this.chartData.oneStar);
+      this.datacollection.datasets[0].data.push(this.chartData.twoStar);
+      this.datacollection.datasets[0].data.push(this.chartData.threeStar);
+      this.datacollection.datasets[0].data.push(this.chartData.fourStar);
+      this.datacollection.datasets[0].data.push(this.chartData.fiveStar);
       console.log("chart data: " + this.datacollection.datasets[0].data);
       this.renderChart(this.datacollection, this.options);
     },
@@ -88,8 +94,4 @@ export default {
   mounted() {
     this.fetchRatings();
   },
-
-}
-  
-
-
+};
